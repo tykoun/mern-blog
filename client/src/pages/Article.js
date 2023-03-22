c6f3564aa19a4aaf84520352def12073fddc1d8c
tykoun <finaritriniaina.manantsoa@gmail.com>
2023-03-22 05:17:33 +0300

    creating and linking the articles list

--- client/src/pages/Article.js
+++ client/src/pages/Article.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
-class Article extends Component {
-    render() {
-        return (
-            <div>
-                <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>This is an Article</h1>
-            </div>
-        );
+import ArticleContent from './Article-content';
+const Article = () => {
+    const { name } = useParams();
+    const article = ArticleContent.find((article) => article.name === name);
+    if (!article) {
+        return <h1>{name} not found</h1>
     }
+    return (
+        <div>
+            <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
+                {article.title}
+            </h1>
+            {article.content.map((paragraph, index) => (
+                <p className='mx-auto leading-relaxed text-base mb-4' key={index}>
+                    {paragraph}
+                </p>
+            ))}
+        </div>
+    );
 }
 
 export default Article;
